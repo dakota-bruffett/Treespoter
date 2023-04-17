58bb73f024103f9f437e57bcd6eccf05b405c265
nq2623up <DakotaBruffett@gmail.com>
2023-04-17 12:50:14 -0500

    Trees

--- app/src/main/java/com/example/trees/MainActivity.kt
+++ app/src/main/java/com/example/trees/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.trees
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.Firebase.firestore
+import java.util.Date
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
+
+        val trees = mapOf("name" to "pine","dateSpotted" to Date)
+
     }
 }
